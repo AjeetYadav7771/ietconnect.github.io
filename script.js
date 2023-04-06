6894e77b3ef164cdc77c344875e3bdfe14b7b5b4
Dev Anand <75121759+heydevanand@users.noreply.github.com>
2023-04-06 09:24:13 +0530

    Added new files for the website
    This is not currently hosted. Just testing the grounds

---
+++ script.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Your JavaScript code goes here
+ 
+  
+const applyNowButton = document.querySelector('button:nth-of-type(1)');
+applyNowButton.addEventListener('click', function() {
+  // Add code here to handle click on "Apply Now" button
+  console.log('Apply Now button clicked');
+});
+
+const aboutUsButton = document.querySelector('button:nth-of-type(2)');
+aboutUsButton.addEventListener('click', function() {
+  // Add code here to handle click on "About Us" button
+  console.log('About Us button clicked');
+});
+
+const admissionsButton = document.querySelector('button:nth-of-type(3)');
+admissionsButton.addEventListener('click', function() {
+  // Add code here to handle click on "Admissions" button
+  console.log('Admissions button clicked');
+});
+
+const academicsButton = document.querySelector('button:nth-of-type(4)');
+academicsButton.addEventListener('click', function() {
+  // Add code here to handle click on "Academics" button
+  console.log('Academics button clicked');
+});
+
+const researchButton = document.querySelector('button:nth-of-type(5)');
+researchButton.addEventListener('click', function() {
+  // Add code here to handle click on "Research" button
+  console.log('Research button clicked');
+});
+
+const campusLifeButton = document.querySelector('button:nth-of-type(6)');
+campusLifeButton.addEventListener('click', function() {
+  // Add code here to handle click on "Campus Life" button
+  console.log('Campus Life button clicked');
+});
+
+const alumniButton = document.querySelector('button:nth-of-type(7)');
+alumniButton.addEventListener('click', function() {
+  // Add code here to handle click on "Alumni" button
+  console.log('Alumni button clicked');
+});
+});
