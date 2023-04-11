0284f84d5cedb817c207b26217930e35d4cf1dbe
Dev Anand <ananddev681@gmail.com>
2023-04-11 08:45:58 +0530

    Playing with Django

--- ietconnect/home/views.py
+++ ietconnect/home/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("This is homepage.")
+    context = {
+        "variable":"created by Dev Anand."
+    }
+    return render(request, "index.html", context)
 
 def about(request):
     return HttpResponse("This is about page.")
